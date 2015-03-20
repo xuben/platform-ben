7d144249b2a3d2d4f3cb24c78dc39c39fd90c70a
xuben <shaci1234@aliyun.com>
2015-03-20 16:39:43 +0800

    Update CountInversions.java

--- src/ben/algorithm/CountInversions.java
+++ src/ben/algorithm/CountInversions.java
@@ -1,4 +1,4 @@
-package ben.algorithm;
+package algorithm;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,52 +8,63 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * count inversions of an input array
+ * @author Ben
+ *
+ */
 public class CountInversions {
-	
+
 	public static void main(String[]args){
 		CountInversions ci = new CountInversions();
-		List<Integer> data = ci.readData("E:/IntegerArray.txt");
-		Integer[] arr = new Integer[data.size()];
+		List<Integer>data = ci.readData("E:/IntegerArray.txt");
+		Integer[]arr = new Integer[data.size()];
 		data.toArray(arr);
-		long count = ci.execute(arr);
-		System.out.print(count);
-	}
-	
-	public long execute(Integer[] arr){
-		if(arr == null || arr.length == 0 || arr.length == 1){
-			return 0;
-		}else{
-			//计算数组中间元素的索引
-			int endIdx = arr.length - 1;
-			int middleIdx = endIdx/2;
-			//将原数组分成左右两个数组
-			Integer[]leftArray = Arrays.copyOfRange(arr, 0, middleIdx+1);
-			Integer[]rightArray = Arrays.copyOfRange(arr, middleIdx+1, endIdx+1);
-			//递归调用计算左右子数组的inversion数
-			long leftCount = execute(leftArray);
-			long rightCount = execute(rightArray);
-			//计算split inversion
-			long splitCount = 0;
-			int i = 0;
-			int j = 0;
-			for(int k = 0;k <= endIdx;k++){
-				if(j >= rightArray.length || 
-						(i < leftArray.length && leftArray[i] <= rightArray[j])){
-					arr[k] = leftArray[i];
-					i++;
-				}else{
-					arr[k] = rightArray[j];
-					splitCount += leftArray.length - i;
-					j++;
-				}
-			}
-			return leftCount+rightCount+splitCount;
-		}
+		System.out.println("inversions:"+ci.execute(arr, 0, arr.length-1));
 	}
 	
 	/**
-	 * 从文件中读取数据
-	 * @param fileName 文件名
+	 * count inversions of arr from beginIndex, to endIndex inclusive
+	 * @param arr
+	 * @param beginIndex
+	 * @param endIndex
+	 * @return
+	 */
+	public long execute(Integer[]arr, int beginIndex, int endIndex){
+		if(arr == null || beginIndex < 0 
+				|| endIndex >= arr.length 
+				|| beginIndex >= endIndex){
+			return 0;
+		}
+		//calculate the middleIndex
+		int middleIndex = (beginIndex+endIndex)/2;
+		//do the recursion twice
+		long leftCount = execute(arr, beginIndex, middleIndex);
+		long rightCount = execute(arr, middleIndex+1, endIndex);
+		//generate copies of left and right part
+		Integer[]leftArray = Arrays.copyOfRange(arr, beginIndex, middleIndex+1);
+		Integer[]rightArray = Arrays.copyOfRange(arr, middleIndex+1, endIndex+1);
+		//calculate the split inversion
+		long splitCount = 0;
+		for(int k = beginIndex, i = 0, j = 0;k <= endIndex;k++){
+			if(j >= rightArray.length 
+					|| (i < leftArray.length 
+							&& leftArray[i] <= rightArray[j])){
+				arr[k] = leftArray[i];
+				i++;
+			}else{
+				arr[k] = rightArray[j];
+				splitCount += leftArray.length - i;
+				j++;
+			}
+		}
+		return leftCount + rightCount + splitCount;
+	}
+	
+	/**
+	 * read input array from file
+	 * @param fileName
+	 * @return
 	 */
 	public List<Integer> readData(String fileName){
 		List<Integer> data = new ArrayList<Integer>();
@@ -61,19 +72,18 @@ public class CountInversions {
 		BufferedReader reader = null;
 		try{
 			reader = new BufferedReader(new FileReader(file));
-			String tempString = null;
-			//一次读入一行，直到读入null为文件结束
-			while((tempString = reader.readLine()) != null){
-				data.add(Integer.parseInt(tempString));
+			String line;
+			//read data line by line, until null
+			while((line = reader.readLine()) != null){
+				data.add(Integer.parseInt(line));
 			}
-			reader.close();
-		}catch(IOException e){
+		}catch(Exception e){
 			e.printStackTrace();
 		}finally{
 			if(reader != null){
 				try{
 					reader.close();
-				}catch(IOException e1){
+				}catch(IOException e1){	
 				}
 			}
 		}
